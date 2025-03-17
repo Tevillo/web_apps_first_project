deeff7fa2508b5f9fc57f9084edc00481c173e03
Paul Borrego <pborrego@trinity.edu>
2025-03-17 15:57:06 -0500

    move

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var inventoryRouter = require('./routes/inventory');
+var canvasRouter = require('./routes/canvas_page');
+
+
 
 var app = express();
 
@@ -20,7 +23,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/inventory', inventoryRouter);
+app.use('/canvas_page', canvasRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
