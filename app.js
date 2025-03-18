cae513f4bd0bc433c4d1855c353742be539543bf
PaulyB <borregopaulj@gmail.com>
2025-03-18 02:04:44 -0500

    done for the night

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var inventoryRouter = require('./routes/inventory');
 var canvasRouter = require('./routes/canvas_page');
-
+var suggestionRouter = require('./routes/suggestions');
 
 
 var app = express();
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/inventory', inventoryRouter);
 app.use('/canvas_page', canvasRouter);
+app.use('/suggestions', suggestionRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
